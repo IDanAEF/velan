24e419cc4876805d07fdfa195644f26b202e9fc1
Denis <efremov89deniss55@gmail.com>
2023-04-07 15:51:06 +0300

    Start

---
+++ assets/es6/blocks/other.js
@@ -0,0 +1,9 @@
+const other = () => {
+    try {
+        
+    } catch (e) {
+        console.log(e.stack);
+    }
+}
+
+export default other;
---
+++ assets/es6/main.js
@@ -0,0 +1,7 @@
+import other from "./blocks/other";
+
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+    other();
+});
